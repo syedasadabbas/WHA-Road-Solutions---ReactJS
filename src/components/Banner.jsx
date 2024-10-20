a67b2b2aac0d05ba605d9c5b79e506236dc6105a
Syed <asadabbassherazi@gmail.com>
2024-10-20 14:52:53 +0500

    Updated Solution

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ function Banner() {
         <div className="container">
           <div className="banner-content">
             <div className="banner-content__text">
-              <h2>Save Big with WHA Road Solutions!</h2>
+              <h2>Save Big with WHA Road Solution!</h2>
               <p>
                 Whether you're looking to purchase, rent, or rent to own, we’ve got unbeatable deals for every option. From top airports to local suppliers, our network offers the best solutions with  <span>24/7</span> support to ensure your journey is seamless and hassle-free.
               </p>
