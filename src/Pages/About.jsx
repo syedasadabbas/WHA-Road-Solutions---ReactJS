79604cecc6d64f5fc9a732d3c3b3d3dd090784a9
Syed <asadabbassherazi@gmail.com>
2024-11-03 02:17:45 +0500

    Removed Images Link From About

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,9 +2,6 @@ import Footer from "../components/Footer";
 import HeroPages from "../components/HeroPages";
 import PlanTrip from "../components/PlanTrip";
 import AboutMain from "../images/about/about-main.jpg";
-import Box1 from "../images/about/icon1.png";
-import Box2 from "../images/about/customer-icon.png";
-import Box3 from "../images/about/icon3.png";
 
 function About() {
   return (
